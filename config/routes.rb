462a3a42973206cb4381a1424a124f9d08cdc7f4
Anuj <anashkan123@gmail.com>
2015-06-15 14:51:40 +0000

    Errors removed. Bug Two root URL solved

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   
   #root 'application#hello'
   
+  
   resources :microposts
   
 
-  resources :users
+  resources :users 
   root 'users#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
